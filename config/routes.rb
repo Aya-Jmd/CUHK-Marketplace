113af66c61b292d0129024f3dd9e63ec5ad93e17
AY-JM <aya.jmoud@epfl.ch>
2026-03-28 18:18:11 +0100

    Added notice when wrong password is issued, in the login page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "search/index"
   get "conversations/index"
   get "conversations/show"
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
   resources :items do
     resources :offers, only: [:create]
   end
